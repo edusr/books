50e0ff2c1ff025866a7524377be9c46c8fbe8068
edusr <edusr@edusr-laptop.(none)>
2013-05-06 13:41:31 -0300

    added contact form

--- views.py
+++ views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from models import Book
+from forms import ContactForm
+from django.core.mail import send_mail
 
 def search_form(request):
     return render(request, 'search_form.html')
@@ -15,3 +17,17 @@ def search(request):
 			books = Book.objects.filter(title__icontains=q)
 			return render(request,'search_results.html',{'books':books,'query':q})
 	return render(request,'search_form.html',{"error":error})
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            send_mail(
+                cd['subject'],
+                cd['message'],
+                cd.get('email', 'noreply@example.com'),['siteowner@example.com'],)
+            return HttpResponseRedirect('/contact/thanks/')
+    else:
+        form = ContactForm(initial={'subject': 'I love your site!'})
+    return render(request, 'contact_form.html', {'form': form})
